08c60aa9460378ee045048141d180652aca1c216
shuall <shualloret@gmail.com>
2015-09-05 14:53:32 -0400

    example of listview update

--- app/src/main/java/com/example/jimmyklein/pillreminder/patientFragTab.java
+++ app/src/main/java/com/example/jimmyklein/pillreminder/patientFragTab.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Waqas on 9/5/2015.
@@ -23,6 +27,17 @@ public class patientFragTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.patient_fragment, container, false);
+        ListView lv = (ListView) v.findViewById(R.id.listView2);
+        List<String> arrlist = new ArrayList<>();
+        arrlist.add("hey");
+        arrlist.add("ho");
+        ArrayAdapter<String> arradapter = new ArrayAdapter<String>(
+                v,
+                android.R.layout.simple_list_item_1,
+                arrlist
+        );
+        lv.setAdapter(arradapter);
+        arrlist.add("what?");
         return v;
 
 
