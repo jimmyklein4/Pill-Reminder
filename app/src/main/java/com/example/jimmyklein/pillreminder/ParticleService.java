d177a66ae0094e790908b3bdda41709a0fb75c4a
shuall <shualloret@gmail.com>
2015-09-05 23:38:59 -0400

    Particle REST api

---
+++ app/src/main/java/com/example/jimmyklein/pillreminder/ParticleService.java
@@ -0,0 +1,21 @@
+package com.example.jimmyklein.pillreminder;
+
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * Created by shua on 9/5/15.
+ */
+public interface ParticleService {
+    @FormUrlEncoded
+    @POST("/{device}/{function}")
+    public void callFunction(
+            @Path("device") String device,
+            @Path("function") String function,
+            @Field("arg") String arg,
+            @Field("access_token") String tok,
+            retrofit.Callback<String> callback);
+}
