833ee409268f9b4fefe8e0058dab9f72184603bc
Waqas <wikkii@msn.com>
2015-09-06 04:06:52 -0400

    hh

--- app/src/main/java/com/example/jimmyklein/pillreminder/DoctorViewAddPatient.java
+++ app/src/main/java/com/example/jimmyklein/pillreminder/DoctorViewAddPatient.java
@@ -2,6 +2,7 @@ package com.example.jimmyklein.pillreminder;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
